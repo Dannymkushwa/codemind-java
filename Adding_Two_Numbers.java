c50d987d1b2009938af0c057211cb8bfddc6921d
Dannymkushwa <107799886+Dannymkushwa@users.noreply.github.com>
2024-07-31 15:32:11 +0530

    import java.util.*; public class Adding{     public static void main(String[] args){         int a, b;         Scanner read=new Scanner(System.in);         a=read.nextInt();         b=read.nextInt();         System.out.print(a+b);     } }

---
+++ Adding_Two_Numbers.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Adding{
+    public static void main(String[] args){
+        int a, b;
+        Scanner read=new Scanner(System.in);
+        a=read.nextInt();
+        b=read.nextInt();
+        System.out.print(a+b);
+    }
+}
